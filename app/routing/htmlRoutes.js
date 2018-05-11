213d060f7844dc886db20012da8573579a6199ba
Dan <dpedersen84@gmail.com>
2018-05-11 18:23:34 -0500

    less, result page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,4 +14,8 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
+    app.get("/result", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/result.html"))
+    })
+
 }
