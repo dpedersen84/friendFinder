c313224e5c6ade7a54edd3d555866c83b555dd95
Dan <dpedersen84@gmail.com>
2018-05-13 10:49:51 -0500

    survey and routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -42,16 +42,53 @@ module.exports = function(app) {
         let newfriend = req.body;
 
         newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
+        console.log(newfriend.scores) // strings
 
-        // console.log(newfriend);
+        newfriend.scores = newfriend.scores.map(function(x) {
+            return parseInt(x)
+        })
+
+        console.log(newfriend.scores); // numbers
 
         // Adds new friend to the array
-        friends.push(newfriend);
+        friends.push(newfriend);    
         // Formats data
         res.json(newfriend);
     })
 
-    app.get("/api/result", function(req, res) {
-        
+    // test
+    app.get("/test", function(req, res) {
+
+        let user1 = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+
+        let user2 = [3, 2, 6, 4, 5, 1, 2, 5, 4, 1];
+
+        let result = user2.map(function(value, index) {
+
+            return Math.abs(value - user1[index]);
+
+        });
+
+        console.log(result);
+        res.json(result);
     })
+
+    // test 2
+    app.get("/test2", function(req, res) {
+
+        let user1 = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+
+        let user2 = [3, 2, 6, 4, 5, 1, 2, 5, 4, 1];
+
+        let result = [];
+
+        for (let i = 0; i < user2.length; i++) {
+            result.push(Math.abs(user2[i] - user1[i]));
+        }
+
+        console.log(result);
+        res.json(result);
+
+    })
+
 };
